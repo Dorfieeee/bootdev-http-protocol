5590037ff8f4df7b2d35c4f5b75f633fd4eb8022
Dorfieeee <rosendorf.dev@gmail.com>
2026-05-06 18:04:10 +0100

    ch4-l2

---
+++ internal/request/request.go
@@ -0,0 +1,107 @@
+package request
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"regexp"
+	"strings"
+)
+
+type Request struct {
+	RequestLine RequestLine
+	ParseState  int
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+const CRLF = "\r\n"
+const INITIALIZED = 0
+const DONE = 1
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.ParseState == DONE {
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	}
+
+	if r.ParseState == INITIALIZED {
+		n, rl, err := parseRequestLine(data)
+		if err == nil && n > 0 {
+			r.RequestLine = *rl
+			r.ParseState = DONE
+		}
+		return n, err
+	}
+
+	return 0, nil
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	r := Request{}
+	buffer := bytes.NewBuffer(make([]byte, 8))
+	for r.ParseState != DONE {
+		chunk := make([]byte, 8)
+		nRead, err := reader.Read(chunk)
+		if err != nil {
+			if buffer.Len() > 0 {
+				buffer.Write(chunk)
+				r.parse(buffer.Bytes())
+				buffer.Reset()
+			}
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			fmt.Printf("Error: %v\n", err)
+			break
+		}
+		buffer.Write(chunk[:nRead])
+		nParsed, err := r.parse(buffer.Bytes())
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			break
+		}
+		if nParsed > 0 {
+			tmp := buffer.Bytes()
+			buffer.Reset()
+			buffer.Write(tmp[nParsed:])
+		}
+	}
+	return &r, nil
+}
+
+func parseRequestLine(data []byte) (int, *RequestLine, error) {
+	idx := bytes.Index(data, []byte(CRLF))
+	if idx == -1 {
+		return 0, nil, nil
+	}
+	rl := strings.Fields(string(data[:idx]))
+	if len(rl) != 3 {
+		return 0, nil, fmt.Errorf("Invalid request line")
+	}
+	method := rl[0]
+	if match, _ := regexp.MatchString("^[A-Z]+$", method); !match {
+		return 0, nil, fmt.Errorf("Invalid HTTP method")
+	}
+	requestTarget := rl[1]
+	httpPart := rl[2]
+	versionPart := strings.Split(httpPart, "/")
+	if len(versionPart) != 2 {
+		return 0, nil, fmt.Errorf("Invalid HTTP version")
+	}
+	if versionPart[0] != "HTTP" {
+		return 0, nil, fmt.Errorf("Invalid HTTP version")
+	}
+	if versionPart[1] != "1.1" {
+		return 0, nil, fmt.Errorf("Invalid HTTP version")
+	}
+	return len(rl), &RequestLine{
+		Method:        method,
+		RequestTarget: requestTarget,
+		HttpVersion:   versionPart[1],
+	}, nil
+}
